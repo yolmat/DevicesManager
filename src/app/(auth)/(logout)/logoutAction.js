ddf5da5051cb8d2c0e1717cc486fe6a74dd37673
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-15 02:36:21 -0300

    Logout delete cookies

--- src/app/(auth)/(logout)/logoutAction.js
+++ src/app/(auth)/(logout)/logoutAction.js
@@ -1,7 +1,9 @@
 'use server'
 
-import { signOut } from "@/auth"
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
 
 export default async function logoutAction() {
-    await signOut
+    (await cookies()).delete("authjs.session-token")
+    return redirect('/')
 }
