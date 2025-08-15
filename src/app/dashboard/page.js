4edf41f0c02a70c5f8e9b984e0153e6a7d583df7
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-15 03:06:39 -0300

    update dashbord page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,7 @@
 import { auth } from "../../../auth"
 import { redirect } from "next/navigation"
+import NavBar from "./navbar"
+import Devices from './devices'
 
 export default async function Dashboard() {
 
@@ -8,5 +10,26 @@ export default async function Dashboard() {
         return redirect('/')
     }
 
-    return <div>Dashboard</div>
+    return (
+        <>
+            <div className="min-h-full">
+                <header as="nav" className="bg-gray-800 dark:bg-gray-800/50">
+                    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                        <NavBar />
+                    </div>
+                </header>
+
+                <header className="relative bg-white shadow-sm dark:bg-gray-800 dark:shadow-none dark:after:pointer-events-none dark:after:absolute dark:after:inset-x-0 dark:after:inset-y-0 dark:after:border-y dark:after:border-white/10">
+                    <div className="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8">
+                        <h1 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Todos os dispositivos</h1>
+                    </div>
+                </header>
+                <main>
+                    <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                        <Devices />
+                    </div>
+                </main>
+            </div>
+        </>
+    )
 }
