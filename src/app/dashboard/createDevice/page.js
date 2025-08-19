5a02c979fbc8c94db926ce67f29db952f796a47b
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-18 23:54:24 -0300

    create page create device

--- src/app/dashboard/createDevice/page.js
+++ src/app/dashboard/createDevice/page.js
@@ -1,3 +1,21 @@
-export default function CreateDevice() {
-    return <div>criar dispositivo</div>
+import Navbar from "../navbar"
+import CreateDeviceForm from './createDeviceForm'
+
+export default async function CreateDevice() {
+    return (
+        <>
+            <Navbar />
+            <div className="flex min-h-full flex-col justify-center px-6 lg:px-8">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900 dark:text-white">
+                        Cadastrar dispositivo
+                    </h2>
+                </div>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <CreateDeviceForm />
+                </ div>
+            </div>
+        </>
+    )
 }
