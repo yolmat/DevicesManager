a114c85e7baa7a1b9af842d3db70d91ea4c19a4e
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-19 00:00:42 -0300

    device action update

--- src/app/dashboard/createDevice/createDeviceAction.js
+++ src/app/dashboard/createDevice/createDeviceAction.js
@@ -7,20 +7,12 @@ export default async function CreateDeviceAction(_prevState, formData) {
     const entries = Array.from(formData.entries())
     const data = Object.fromEntries(entries)
 
-    console.log(data)
-
     if (!data.userDevice || !data.deviceType || !data.device || !data.serialNumber) {
         return {
             message: "Preencha todos os campos",
             success: false
         }
     }
-
-    console.log(data.userDevice)
-    console.log(data.deviceType)
-    console.log(data.device)
-    console.log(data.serialNumber)
-
 
     const device = await db.devices.findUnique({
         where: {
@@ -41,8 +33,7 @@ export default async function CreateDeviceAction(_prevState, formData) {
             deviceType: data.deviceType,
             serialNumber: data.serialNumber,
             userDevice: data.userDevice,
-            status: false,
-            historys: ""
+            status: false
         }
     })
 
