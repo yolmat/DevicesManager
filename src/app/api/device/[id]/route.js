96df9525a234ceb0fbf265f86ce0b299f0dc942a
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-21 03:03:22 -0300

    Create device uniq router

---
+++ src/app/api/device/[id]/route.js
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import db from "../../../../lib/db"
+
+export async function GET(request, { params }) {
+
+    const { id } = params
+
+    try {
+        const device = await db.devices.findFirst({
+            where: {
+                id: Number(id)
+            }
+        })
+
+        if (!device) {
+            return NextResponse.json(
+                { error: "dispositivo não encontrado" },
+                { status: 404 }
+            )
+        }
+        return NextResponse.json(device)
+    } catch (e) {
+        return NextResponse.json({ error: 'Dados não encontrados' }, { status: 500 })
+    }
+}
