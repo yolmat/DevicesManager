003f4edf69ba740e761239c12c2c2d3cbcda1aef
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-23 00:19:38 -0300

    fixed: delete token

--- src/app/(auth)/(logout)/logoutAction.js
+++ src/app/(auth)/(logout)/logoutAction.js
@@ -4,6 +4,6 @@ import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
 export default async function logoutAction() {
-    (await cookies()).delete("authjs.session-token")
+    (await cookies()).delete("__Secure-authjs.session-token")
     return redirect('/')
 }
