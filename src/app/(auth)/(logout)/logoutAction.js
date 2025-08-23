33386338a80f3012ca6623292a7ac5e30dad658a
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-23 19:55:46 -0300

    fixed: logout error

--- src/app/(auth)/(logout)/logoutAction.js
+++ src/app/(auth)/(logout)/logoutAction.js
@@ -4,6 +4,13 @@ import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
 
 export default async function logoutAction() {
-    (await cookies()).delete("__Secure-authjs.session-token")
+    if ((await cookies()).get("__Secure-authjs.session-token")) {
+        (await cookies()).delete("__Secure-authjs.session-token")
+    }
+
+    if ((await cookies()).get("authjs.session-token")) {
+        (await cookies()).delete("authjs.session-token")
+    }
+
     return redirect('/')
 }
