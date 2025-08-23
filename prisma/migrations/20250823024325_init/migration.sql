a99a7864394e7bb4bf665a552ff62be8f5ff319f
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-22 23:59:54 -0300

    new migrate

---
+++ prisma/migrations/20250823024325_init/migration.sql
@@ -0,0 +1,53 @@
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."Devices" (
+    "id" SERIAL NOT NULL,
+    "device" TEXT NOT NULL,
+    "deviceType" TEXT NOT NULL,
+    "serialNumber" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "status" BOOLEAN NOT NULL,
+    "userDevice" TEXT NOT NULL,
+    "Qrcode" TEXT NOT NULL,
+
+    CONSTRAINT "Devices_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."History" (
+    "id" SERIAL NOT NULL,
+    "devicesId" INTEGER,
+    "comments" TEXT NOT NULL,
+
+    CONSTRAINT "History_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "public"."User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Devices_id_key" ON "public"."Devices"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Devices_serialNumber_key" ON "public"."Devices"("serialNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "History_id_key" ON "public"."History"("id");
+
+-- AddForeignKey
+ALTER TABLE "public"."History" ADD CONSTRAINT "History_devicesId_fkey" FOREIGN KEY ("devicesId") REFERENCES "public"."Devices"("id") ON DELETE SET NULL ON UPDATE CASCADE;
