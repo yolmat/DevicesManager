d0f5ae2161c207df853ece41a88fc47a40e6ac17
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-22 23:40:46 -0300

    fixed: QRCode Params

--- prisma/migrations/20250822034055_create_historys/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `historys` to the `History` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "public"."History" ADD COLUMN     "historys" TEXT NOT NULL;
