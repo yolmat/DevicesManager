d2dc6892201b6a39bfa319f7d4f641ae5cae4eba
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-23 22:43:17 -0300

    test: bug create device

--- src/app/dashboard/createDevice/createDeviceAction.js
+++ src/app/dashboard/createDevice/createDeviceAction.js
@@ -1,39 +1,33 @@
 'use server'
 
 import { redirect } from "next/navigation"
+import { headers } from "next/headers"
 
 export default async function CreateDeviceAction(_prevState, formData) {
     const data = Object.fromEntries(Array.from(formData.entries()))
+    console.log("Form data:", data)
 
-    console.log(data)
+    // Pega o host real da request
+    const h = await headers()
+    const host = h.get("host") // exemplo: msaraiva.dev.br ou localhost:3000
+    const protocol = process.env.NODE_ENV === "production" ? "https" : "http"
+    const baseUrl = `${protocol}://${host}`
 
-    // Resolve base URL automaticamente
-    const baseUrl = process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}`
-        : "http://localhost:3000"
-
-    console.log(baseUrl)
+    console.log("Base URL:", baseUrl)
 
     const res = await fetch(`${baseUrl}/api/devices/create`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
+        cache: "no-store" // garante que sempre vai buscar o endpoint atualizado
     })
 
-    console.log(res)
-
-
     const result = await res.json()
-
-    console.log(result)
+    console.log("API result:", result)
 
     if (result.success) {
         return redirect("/dashboard")
     }
 
-    if (!result.success) {
-        return { success: false, message: result.message }
-    }
-
-
+    return { success: false, message: result.message }
 }
