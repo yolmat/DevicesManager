32f2c0b5b4675c3a72411367d9ffcb7cc74473d9
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-23 22:30:48 -0300

    test: bug create device

--- src/app/dashboard/createDevice/createDeviceAction.js
+++ src/app/dashboard/createDevice/createDeviceAction.js
@@ -5,10 +5,14 @@ import { redirect } from "next/navigation"
 export default async function CreateDeviceAction(_prevState, formData) {
     const data = Object.fromEntries(Array.from(formData.entries()))
 
+    console.log(data)
+
     // Resolve base URL automaticamente
     const baseUrl = process.env.VERCEL_URL
         ? `https://${process.env.VERCEL_URL}`
         : "http://localhost:3000"
+
+    console.log(baseUrl)
 
     const res = await fetch(`${baseUrl}/api/devices/create`, {
         method: "POST",
@@ -16,11 +20,20 @@ export default async function CreateDeviceAction(_prevState, formData) {
         body: JSON.stringify(data)
     })
 
+    console.log(res)
+
+
     const result = await res.json()
+
+    console.log(result)
+
+    if (result.success) {
+        return redirect("/dashboard")
+    }
 
     if (!result.success) {
         return { success: false, message: result.message }
     }
 
-    return redirect("/dashboard")
+
 }
