c40e68456d2ee62d10576fee57f4619790c18f64
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-28 00:36:00 -0300

    ajust styles login

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+import { tv } from "tailwind-variants"
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx}",
+        "./shadcn/**/*.{js,ts,jsx,tsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
