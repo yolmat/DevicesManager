fa11ea2bc4ce4ec4d02c63ce0088024a52138fb4
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-28 03:08:51 -0300

    Create login form and styles

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,52 +1,107 @@
-import { cn } from "@/src/lib/utils"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+"use client";
+import { cn } from "@/src/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useForm } from "react-hook-form";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "./ui/form";
+import loginAction from "@/src/app/(auth)/login/loginAction";
+import Link from "next/link";
+
+type TformLogin = {
+  email: string;
+  password: string;
+};
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"form">) {
+  const form = useForm<TformLogin>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function handleSubmitLogin(data: TformLogin) {
+    loginAction(data.email, data.password);
+    console.log(await loginAction(data.email, data.password));
+  }
+
   return (
-    <form className={cn("flex flex-col gap-6", className)} {...props}>
-      <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Entre com a sua conta</h1>
-        <p className="text-muted-foreground text-sm text-balance">
-          Digite seu e-mail abaixo para acessar sua conta
-        </p>
-      </div>
-      <div className="grid gap-6">
-        <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="exemplo@email.com" required />
+    <Form {...form}>
+      <form
+        className={cn("flex flex-col gap-6", className)}
+        onSubmit={form.handleSubmit(handleSubmitLogin)}
+      >
+        <div className="flex flex-col items-center gap-2 text-center">
+          <h1 className="text-2xl font-bold">Entre com a sua conta</h1>
+          <p className="text-muted-foreground text-sm text-balance">
+            Digite seu e-mail abaixo para acessar sua conta
+          </p>
         </div>
-        <div className="grid gap-3">
-          <div className="flex items-center">
-            <Label htmlFor="password">Senha</Label>
-            <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Esqueceu sua senha ?
-            </a>
+        <div className="grid gap-6">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem className="grid gap-3">
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <FormControl>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="exemplo@email.com"
+                    required
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem className="grid gap-3">
+                <div className="flex items-center">
+                  <FormLabel htmlFor="password">Senha</FormLabel>
+                  <a
+                    href="#"
+                    className="ml-auto text-sm underline-offset-4 hover:underline"
+                  >
+                    Esqueceu sua senha ?
+                  </a>
+                </div>
+                <FormControl>
+                  <Input
+                    id="password"
+                    type="password"
+                    placeholder="********"
+                    required
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="w-full">
+            Entre
+          </Button>
+          <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+            <span className="bg-background text-muted-foreground relative z-10 px-2">
+              Ou continue aqui
+            </span>
           </div>
-          <Input id="password" type="password" placeholder="********" required />
         </div>
-        <Button type="submit" className="w-full">
-          Entre
-        </Button>
-        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-          <span className="bg-background text-muted-foreground relative z-10 px-2">
-            Ou continue aqui
-          </span>
+        <div className="text-center text-sm">
+          Ainda não tem uma conta?{" "}
+          <Link href="/cadastro" className="underline underline-offset-4">
+            Inscreva-se
+          </Link>
         </div>
-      </div>
-      <div className="text-center text-sm">
-        Ainda não tem uma conta?{" "}
-        <a href="#" className="underline underline-offset-4">
-          Inscreva-se
-        </a>
-      </div>
-    </form>
-  )
+      </form>
+    </Form>
+  );
 }
