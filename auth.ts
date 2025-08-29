6906aff7f779fd1ad0e02d80dd19f0707c0b39f5
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-29 03:44:30 -0300

    Fixed routes.ts types

--- auth.ts
+++ auth.ts
@@ -1,19 +1,30 @@
-import { findUserByCredentials } from "./src/lib/user"
-import NextAuth from "next-auth"
-import Credentials from "next-auth/providers/credentials"
+import { findUserByCredentials } from "./src/lib/user";
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [
-        Credentials({
-            credentials: {
-                email: {},
-                password: {},
-            },
-            authorize: async (credentials) => {
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        if (
+          !credentials ||
+          typeof credentials.email !== "string" ||
+          typeof credentials.password !== "string"
+        ) {
+          return null;
+        }
 
-                const user = await findUserByCredentials(credentials.email, credentials.password)
+        const user = await findUserByCredentials(
+          credentials.email,
+          credentials.password
+        );
 
-                return user
-            }
-        })],
-})
+        return user;
+      },
+    }),
+  ],
+});
