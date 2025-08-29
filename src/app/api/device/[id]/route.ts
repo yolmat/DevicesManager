db5062aabb36e24a9c72a2463e58b3ab9d409a37
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-29 03:21:34 -0300

    Tranform routes js in routes ts

---
+++ src/app/api/device/[id]/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import db from "../../../../lib/db";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const id = params.id;
+
+  try {
+    const device = await db.devices.findFirst({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!device) {
+      return NextResponse.json(
+        { error: "dispositivo não encontrado" },
+        { status: 404 }
+      );
+    }
+    return NextResponse.json(device);
+  } catch (e) {
+    return NextResponse.json(
+      { error: "Dados não encontrados" },
+      { status: 500 }
+    );
+  }
+}
