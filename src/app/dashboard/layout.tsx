680f07aae2f91b82db542f4be6b65159fea30676
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-31 05:29:22 -0300

    upgrade all main page dashboard, layout, devices and navbar. Js to Ts

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,26 @@
+import Navbar from "./navbar";
+import { auth } from "../../../auth";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+  if (!session) {
+    return redirect("/");
+  }
+
+  return (
+    <section className="min-h-full">
+      <header className="bg-background">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <Navbar />
+        </div>
+      </header>
+
+      {children}
+    </section>
+  );
+}
