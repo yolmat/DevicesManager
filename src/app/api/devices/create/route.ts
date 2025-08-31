0d6858d97d2de8b34fd726f6a23d90bc904816d8
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-08-31 05:27:48 -0300

    Upgrade js to ts and fixed typing

---
+++ src/app/api/devices/create/route.ts
@@ -0,0 +1,38 @@
+import db from "../../../../lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const data = await req.json();
+
+  if (!data.userDevice || !data.device || !data.serialNumber) {
+    return NextResponse.json({
+      success: false,
+      message: "Preencha todos os campos",
+    });
+  }
+
+  const exists =
+    (await db.devices.findUnique({
+      where: { serialNumber: data.serialNumber },
+    })) !== null;
+
+  if (exists) {
+    return NextResponse.json({
+      success: false,
+      message: "Esse dispositivo já existe",
+    });
+  }
+
+  await db.devices.create({
+    data: {
+      device: data.device,
+      deviceType: data.deviceType,
+      serialNumber: data.serialNumber,
+      userDevice: data.userDevice,
+      status: false,
+      Qrcode: "",
+    },
+  });
+
+  return NextResponse.json({ success: true });
+}
