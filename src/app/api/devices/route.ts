ecde78290767b32f0e47794752f5d38da9ba31a8
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-09-06 02:59:26 -0300

    where createdBy

--- src/app/api/devices/route.ts
+++ src/app/api/devices/route.ts
@@ -1,12 +1,23 @@
 import { NextResponse } from "next/server";
 import db from "../../../lib/db";
+import { cookies } from "next/headers";
 
 export async function GET() {
+  const cookieStore = await cookies();
+  const myNumberStr = cookieStore.get("createId")?.value;
+  const myNumber = Number(myNumberStr);
 
-    try {
-        const devices = await db.devices.findMany()
-        return NextResponse.json(devices)
-    } catch (e) {
-        return NextResponse.json({ error: 'Dados não encontrados' }, { status: 500 })
-    }
+  try {
+    const devices = await db.devices.findMany({
+      where: {
+        createdBy: myNumber,
+      },
+    });
+    return NextResponse.json(devices);
+  } catch (e) {
+    return NextResponse.json(
+      { error: "Dados não encontrados" },
+      { status: 500 }
+    );
+  }
 }
