b353e1983e2b700d82bf98ec43bb03e5194bafff
Mateus Saraiva <mateus.saraiva09@outlook.com>
2025-09-06 03:00:30 -0300

    upgrade lib user

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,7 +1,11 @@
+"use server";
 import db from "./db";
 import bcrypt from "bcrypt";
+import { cookies } from "next/headers";
 
 export async function findUserByCredentials(email: string, password: string) {
+  const cookiesStore = await cookies();
+
   const user = await db.user.findFirst({
     where: {
       email: email,
@@ -15,7 +19,13 @@ export async function findUserByCredentials(email: string, password: string) {
   const passwordMatch = bcrypt.compareSync(password, user.password);
 
   if (passwordMatch) {
+    cookiesStore.set("createId", user.id.toString(), {
+      httpOnly: true,
+      path: "/",
+    });
+
     return {
+      id: user.id,
       name: user.name,
       email: user.email,
     };
